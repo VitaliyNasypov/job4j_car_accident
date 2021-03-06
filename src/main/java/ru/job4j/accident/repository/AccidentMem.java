8e0540c03e61cd9ebfad98d0b1f7a94b4feb9a95
Vitaliy Nasypov <nasvit25@gmail.com>
2021-03-06 14:22:49 +0700

    Fixed #408669

--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -3,6 +3,7 @@ package ru.job4j.accident.repository;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.Accident;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -27,8 +28,8 @@ public class AccidentMem {
         return Lazy.INST;
     }
 
-    public Map<Integer, Accident> findAll() {
-        return ACCIDENTS;
+    public Collection<Accident> findAll() {
+        return ACCIDENTS.values();
     }
 
     public Accident create(Accident accident) {
