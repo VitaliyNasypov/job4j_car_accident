b20fca31001e5514018183faa87dbeed4e01029b
Vitaliy Nasypov <nasvit25@gmail.com>
2021-03-07 20:15:02 +0700

    6. Сборка war [#408668]

--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -15,7 +15,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Repository
 public class AccidentMem {
     private static final AtomicInteger ACCIDENT_ID = new AtomicInteger(4);
-    private final Map<Integer, Accident> ACCIDENTS = new ConcurrentHashMap<>();
+    private final Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
 
     private AccidentMem() {
         Rule one = Rule.of(1, "Article. 1");
@@ -23,13 +23,13 @@ public class AccidentMem {
         Set<Rule> rules = new HashSet<>();
         rules.add(one);
         rules.add(two);
-        ACCIDENTS.put(1, new Accident(1, "Name_1", "Text_1", "Address_1",
+        accidents.put(1, new Accident(1, "Name_1", "Text_1", "Address_1",
                 AccidentType.of(1, "Две машины"), rules));
-        ACCIDENTS.put(2, new Accident(2, "Name_2", "Text_2", "Address_2",
+        accidents.put(2, new Accident(2, "Name_2", "Text_2", "Address_2",
                 AccidentType.of(1, "Две машины"), rules));
-        ACCIDENTS.put(3, new Accident(3, "Name_3", "Text_3", "Address_3",
+        accidents.put(3, new Accident(3, "Name_3", "Text_3", "Address_3",
                 AccidentType.of(3, "Машина и велосипед"), rules));
-        ACCIDENTS.put(4, new Accident(4, "Name_4", "Text_4", "Address_4",
+        accidents.put(4, new Accident(4, "Name_4", "Text_4", "Address_4",
                 AccidentType.of(1, "Две машины"), rules));
     }
 
@@ -42,17 +42,17 @@ public class AccidentMem {
     }
 
     public Collection<Accident> findAll() {
-        return ACCIDENTS.values();
+        return accidents.values();
     }
 
     public Accident create(Accident accident) {
         if (accident.getId() == 0) {
             accident.setId(ACCIDENT_ID.incrementAndGet());
         }
-        return ACCIDENTS.put(accident.getId(), accident);
+        return accidents.put(accident.getId(), accident);
     }
 
     public Accident findById(int id) {
-        return ACCIDENTS.get(id);
+        return accidents.get(id);
     }
 }
